b807168d36322f1fce8de55f95eb6efc15f6c76f
yadonoya <mscottschreiber@gmail.com>
2023-11-01 22:38:13 +0900

    feat: Add components to main App, and prepared basic skeleton of webapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-export default function App() {}
+export default function App() {
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
+}
 
 function Logo() {
+  //Add emojis to the title of h1 later. Not working right now for some reason
   return <h1>Far Away</h1>;
 }
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
