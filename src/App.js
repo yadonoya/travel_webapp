f0d82bafcdd597a1713f885ddfbe7b8f210a77de
yadonoya <mscottschreiber@gmail.com>
2023-11-01 22:43:04 +0900

    fix: add className to footer tag in Stats component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function PackingList() {
 }
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
