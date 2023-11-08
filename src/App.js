a47e68e03955d9f3d34a0a7aab8dffd9668277db
yadonoya <mscottschreiber@gmail.com>
2023-11-08 22:41:47 +0900

    feat: add handleSubmit funtionality to Form component

--- src/App.js
+++ src/App.js
@@ -19,10 +19,22 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +42,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
