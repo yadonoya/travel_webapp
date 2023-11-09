ff62e26f4ea9c26abb51190869cf19fcfd1d9b01
yadonoya <mscottschreiber@gmail.com>
2023-11-09 22:35:51 +0900

    feat: handle Form component to revert to default state values upon submission

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
