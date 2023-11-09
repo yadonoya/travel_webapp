b8e0fd6cfd1713dc401dc4e5319c8f1bb59febfb
yadonoya <mscottschreiber@gmail.com>
2023-11-09 22:27:25 +0900

    feat: add controlled element for select attribute within Form component

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function Logo() {
 }
 function Form() {
   const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -30,7 +31,7 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
