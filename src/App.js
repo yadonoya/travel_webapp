5f2f8ce5d73cfd4474bbc290ea8f7219d551276a
yadonoya <mscottschreiber@gmail.com>
2023-11-09 22:21:17 +0900

    feat: add controlled element for input field in Form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
@@ -19,6 +21,8 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
   }
@@ -33,7 +37,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
