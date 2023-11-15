cd9bde1ce42999c0b3112d5b3a7c3de2c960ba3c
yadonoya <mscottschreiber@gmail.com>
2023-11-15 11:29:51 +0900

    feat: dynamically caluclate percentage of items packed in Stats component

--- src/App.js
+++ src/App.js
@@ -114,12 +114,15 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 function Stats({ items }) {
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
+  const percentagePacked = !numItems
+    ? '0'
+    : Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className="stats">
       <em>
         You have {numItems} {numItems === 1 ? 'item' : 'items'} on your list,
-        and you already packed {numPacked} (X%)
+        and you already packed {numPacked} ({percentagePacked}%)
       </em>
     </footer>
   );
